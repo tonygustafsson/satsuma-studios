21e972c3c58bc03f321b6b465d073d0b13bdb8f1
unknown <Tony.Gustafsson@Tony-HP.sr.local>
2016-02-03 15:16:07 +0100

    Scroll fix, did not work anymore. Needs to be fixed.

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -3,10 +3,16 @@
 	"use strict";
 
 	var speed = 500,
+    currentUrl = window.location.href,
 	moving_frequency = 15,
 	links = document.getElementsByTagName('a'),
 	href,
 	i;
+
+    if (currentUrl.indexOf('.html') > -1) {
+        // Only run on start page
+        return;
+    }
 
 	var getScrollTopElement = function(e) {
 		var top = 0;
@@ -21,13 +27,12 @@
 
 	for (i = 0; i < links.length; i = i + 1) {   
 	    href = (links[i].attributes.href === undefined) ? null : links[i].attributes.href.value.toString();
-
-		if (href !== null && href.length > 1 && href.substr(0, 1) === '#') {
+		if (href !== null && href.length > 1 && href.indexOf('#') > -1) {
 			links[i].onclick = function() {
 				var element,
 				href = this.attributes.href.value.toString();
 
-				if (element = document.getElementById(href.substr(1))) {
+				if (element = document.getElementById(href.substr(23))) {
 					var hop_count = speed / moving_frequency,
 					getScrollTopDocumentAtBegin = document.documentElement.scrollTop + document.body.scrollTop,
 					gap = (getScrollTopElement(element) - getScrollTopDocumentAtBegin) / hop_count,
