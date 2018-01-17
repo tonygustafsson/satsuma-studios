e09eb2009b9fa5abe879082dd9480e41d9001905
Tony Gustafsson <mail@tonyg.se>
2018-01-17 21:19:14 +0100

    New page, new posibilities

--- gulpfile.js
+++ gulpfile.js
@@ -1,76 +1,23 @@
 const gulp = require('gulp');
-const fs = require("fs");
-const fileinclude = require('gulp-file-include');
-const markdown = require('marked');
-
-const postcss = require('gulp-postcss');
-const cssimport = require('postcss-import');
-const cssnext = require("postcss-cssnext")
-const simplevars = require('postcss-simple-vars');
-const nestedcss = require('postcss-nested');
-const mqpacker = require('css-mqpacker');
-const cssmin = require('gulp-cssmin');
-
-const babel = require('gulp-babel');
-const uglify = require('gulp-uglify');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 const concat = require('gulp-concat');
 
-gulp.task('html', function () {
-	return gulp.src(['./src/html/**/*.html'])
-		.pipe(fileinclude({
-            prefix: '@@',
-            basepath: './src/html/',
-            filters: {
-                markdown: markdown.parse
-            },
-            context: {
-                year: new Date().getFullYear(),
-                baseurl: 'http://www.satsuma.se/',
-                csspath: '../css',
-                jspath: '../js',
-                imgpath: '../img'
-            }
+gulp.task('sass', function () { 
+    return gulp.src('./src/sass/main.scss')
+        .pipe(sass({
+            outputStyle: 'compressed'
+        })).on('error', sass.logError)
+        .pipe(autoprefixer({
+            browsers: ["last 2 versions"]
         }))
-		.pipe(gulp.dest('app'));
-});
-
-gulp.task('html:watch', function () {
-    gulp.watch(['./src/html/**/*.html', './src/md/**/*.md', './src/html-templates/**/*.html'], ['html']);
-});
-
-gulp.task('css', function () {
-    var processors = [
-        cssnext,
-        cssimport,
-        nestedcss,
-        simplevars,
-        mqpacker
-    ];
-    
-    gulp.src('./src/css/**/*.css')
         .pipe(concat('main.css'))
-        .pipe(postcss(processors))
-        .pipe(cssmin())
-        .pipe(gulp.dest('./app/css'));
+        .pipe(gulp.dest('./assets/css'));
 });
 
-gulp.task('css:watch', function () {
-    gulp.watch('./src/css/**/*.css', ['css']);
-});
- 
-gulp.task('js', function(){
-  return gulp.src('./src/js/*.js')
-    .pipe(babel({
-        presets: ['es2015']
-    }))
-    .pipe(concat('main.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('app/js'))
+gulp.task('sass:watch', function () {
+    gulp.watch(['./src/sass/**/*.scss'], ['sass']);
 });
 
-gulp.task('js:watch', function () {
-    gulp.watch('./src/js/**/*.js', ['js']);
-});
-
-gulp.task('watch', ['html:watch', 'css:watch', 'js:watch']);
-gulp.task('default', ['html', 'css', 'js']);
+gulp.task('watch', ['sass:watch']);
+gulp.task('default', ['sass']);
