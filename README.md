78da66eb021acb0ef44d38ba5c10b55a88c3768e
Tony Gustafsson <tony.gustafsson@starrepublic.com>
2021-11-07 12:51:53 +0100

    Update gulp and packages

---
+++ README.md
@@ -0,0 +1,3 @@
+# Satsuma Studios
+
+This is just a repo of a static site hosted on www.satsuma.se.
--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,22 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const autoprefixer = require('gulp-autoprefixer');
-const concat = require('gulp-concat');
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const autoprefixer = require("gulp-autoprefixer");
+const concat = require("gulp-concat");
 
-gulp.task('sass', function () { 
-    return gulp.src('./src/sass/main.scss')
-        .pipe(sass({
-            outputStyle: 'compressed'
-        })).on('error', sass.logError)
-        .pipe(autoprefixer({
-            browsers: ["last 2 versions"]
-        }))
-        .pipe(concat('main.css'))
-        .pipe(gulp.dest('./assets/css'));
-});
+gulp.task("default", () =>
+  gulp
+    .src("./src/sass/main.scss")
+    .pipe(
+      sass({
+        outputStyle: "compressed",
+      })
+    )
+    .on("error", sass.logError)
+    .pipe(autoprefixer())
+    .pipe(concat("main.css"))
+    .pipe(gulp.dest("./assets/css"))
+);
 
-gulp.task('sass:watch', function () {
-    gulp.watch(['./src/sass/**/*.scss'], ['sass']);
-});
-
-gulp.task('watch', ['sass:watch']);
-gulp.task('default', ['sass']);
+gulp.task("watch", () =>
+  gulp.watch("./src/sass/**/*.scss", gulp.series(["default"]))
+);
